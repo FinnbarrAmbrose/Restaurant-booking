a6bb901f318900f66a5d4875b27bcb634008ea80
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2025-03-14 22:18:00 +0000

    Add Booking model and register it in admin

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link booking to a user
+    date = models.DateField()  # Booking date
+    time = models.TimeField()  # Booking time
+    guests = models.PositiveIntegerField()  # Number of guests
+    created_at = models.DateTimeField(auto_now_add=True)  # Auto timestamp when created
