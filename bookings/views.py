c6b843d29f89f536e8e5d6bef5a8f9b7e60b9625
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2025-03-15 10:00:02 +0000

    Add booking_list view to display user's bookings sorted by date and time

--- bookings/views.py
+++ bookings/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "bookings/home.html")
+
+
+def booking_list(request):
+    bookings = Booking.objects.filter(user=request.user).order_by('date', 'time')
+    return render(request, "bookings/booking_list.html", {"bookings": bookings})
+
+
+
