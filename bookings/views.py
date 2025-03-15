3f42b8da22f74cc520f6ca2779f35365c1326284
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2025-03-15 10:21:36 +0000

    Add booking_update view to handle updates to existing bookings

--- bookings/views.py
+++ bookings/views.py
@@ -25,3 +25,19 @@ def booking_create(request):
     else:
         form = BookingForm()
     return render(request, "bookings/booking_form.html", {"form": form})
+
+
+
+def booking_update(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id, user=request.user)
+
+    if request.method == "POST":
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            form.save()
+            return redirect("booking_list")  # Redirect to booking list
+    else:
+        form = BookingForm(instance=booking)
+
+    return render(request, "bookings/booking_form.html", {"form": form})
+
