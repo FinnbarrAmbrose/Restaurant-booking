9052258d058022cdf8564dafae7ade867fd07618
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2025-03-15 10:04:06 +0000

    Add booking_create view to handle new booking submissions

--- bookings/views.py
+++ bookings/views.py
@@ -12,3 +12,15 @@ def booking_list(request):
 
 
 
+
+def booking_create(request):
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user  # Assign booking to the logged-in user
+            booking.save()
+            return redirect("booking_list")  # Redirect to booking list
+    else:
+        form = BookingForm()
+    return render(request, "bookings/booking_form.html", {"form": form})
