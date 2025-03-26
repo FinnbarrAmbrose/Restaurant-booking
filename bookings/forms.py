30358274854c6adde73cf1106b456a1619b83856
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2025-03-26 18:20:40 +0000

    number of guests is between 1 and 4

--- bookings/forms.py
+++ bookings/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Booking
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from datetime import date
 
 class BookingForm(forms.ModelForm):
     class Meta:
@@ -12,6 +14,12 @@ class BookingForm(forms.ModelForm):
             'time': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
             'guests': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 4}), # Limit the guests to 4 
         }
+
+        def clean_guests(self):
+            guests = self.cleaned_data.get('guests')
+            if guests < 1 or guests > 4:
+                raise ValidationError("Each table must have between 1 and 4 guests.")
+            return guests
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
