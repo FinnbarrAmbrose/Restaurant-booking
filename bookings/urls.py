e7b22de517cb7a5ea71a9c1b46f0e316c9e9dfc7
FinnbarrAmbrose <finnbarr.ambrose121@gmail.com>
2025-07-10 18:21:16 +0000

    Remove redundant booking creation path and update contact path in URLs

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import home, booking_list ,booking_update , booking_create, booking_delete, register, user_login, user_logout, contact_restaurant
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('booking/create/', home, name='booking_create'),
+    path('', home, name='home')
     path("bookings/", booking_list, name="booking_list"),
     path("bookings/new/", booking_create, name="booking_create"),
     path("bookings/edit/<int:booking_id>/", booking_update, name="booking_update"),
@@ -11,6 +10,5 @@ urlpatterns = [
     path("register/", register, name="register"),
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
-    path("contact/", contact_restaurant, name="contact_restaurant"),
     path("contact/<int:booking_id>/", contact_restaurant, name="contact_booking"),
 ]
