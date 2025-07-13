db204e28747a42e9dd9bee159f225988b41cfcc1
FinnbarrAmbrose <finnbarr.ambrose121@gmail.com>
2025-07-13 12:34:40 +0000

    Set DEBUG to True for development environment

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -32,7 +32,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ["localhost",
                  "127.0.0.1",
